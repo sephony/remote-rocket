92a74c65ce696020c6e3eb4ea299e50eca204e72
sephony <3266546058@qq.com>
2024-06-02 16:23:42 +0800

    ✨ feat: 添加三维图关机点绘制

--- visualizeRocketData.m
+++ visualizeRocketData.m
@@ -1,7 +1,17 @@
 function visualizeRocketData(display, X_powered, t_powered, X_whole)
 %% 绘制主动段弹道曲线（发射坐标系下）
+% 获取各级关机点时间对应的索引
+idx_stage1 = find(t_powered == display.t_stage(1), 1);
+idx_stage2 = find(t_powered == display.t_stage(1) + display.t_stage(2), 1);
+idx_stage3 = find(t_powered == display.t_stage(1) + display.t_stage(2) + display.t_stage(3), 1);
+vec_idx = [idx_stage1, idx_stage2, idx_stage3];
+
 figure (1);
+hold on
 plot3(X_powered(:,1),X_powered(:,3),X_powered(:,2));
+plotBornoutPoint3(X_powered, vec_idx);
+hold off
+view(3);
 axis equal;
 grid on;
 xlabel('x/m');
@@ -11,7 +21,11 @@ title('发射坐标系下主动段弹道曲线');
 
 %% 绘制全弹道曲线（发射坐标系下）
 figure (2);
+hold on
 plot3(X_whole(:,1),X_whole(:,3),X_whole(:,2));
+plotBornoutPoint3(X_whole, vec_idx);
+hold off
+view(3);
 axis equal;
 grid on;
 xlabel('x/m');
@@ -22,18 +36,19 @@ title('发射坐标系下全弹道曲线');
 R = X_whole(:,1:3);
 % 所有时刻的地心坐标系下火箭地心矢量
 R_E = Rotation.L2E(display.A_L0, display.theta_T0, display.Phi_T0) * R' + display.R0_e;
-
+R_E = R_E';
 %% 在地球上可视化弹道曲线（地心坐标系下）
 figure(3);
-traj = plot3(R_E(1,:), R_E(2,:), R_E(3,:));
+hold on
+traj = plot3(R_E(:,1), R_E(:,3), R_E(:,2));
 traj.LineWidth = 3;
 view(3);
-hold on
 ellipsoid(0, 0, 0, Earth.a_e, Earth.a_e, Earth.b_e);
+plotBornoutPoint3(R_E, vec_idx);
+hold off
 axis equal
 grid on
 title('地心坐标系下弹道曲线');
-hold off
 
 %% 可视化火箭主动段各参数数据
 % 计算主动段数据长度
@@ -63,11 +78,7 @@ for i = 1:size(t_powered,1)
     Phi_L_display(i) =  Earth.rad2deg(display.Phi_L);
     n_display(i) = display.n;
 end
-% 获取各级关机点时间对应的索引
-idx_stage1 = find(t_powered == display.t_stage(1), 1);
-idx_stage2 = find(t_powered == display.t_stage(1) + display.t_stage(2), 1);
-idx_stage3 = find(t_powered == display.t_stage(1) + display.t_stage(2) + display.t_stage(3), 1);
-vec_idx = [idx_stage1, idx_stage2, idx_stage3];
+
 %% 绘制一级飞行时最大攻角、最大动压和最大法向过载
 % 计算一级飞行最大攻角、最大动压和最大法向过载
 [max_alpha, idx_max_alpha] = min(alpha_display(1:idx_stage1));
@@ -80,7 +91,7 @@ fprintf('一级飞行时最大动压: %fPa 在时间: %fs\n', max_q, t_powered(i
 fprintf('一级飞行时最大法向过载: %fg 在时间: %fs\n', max_n, t_powered(idx_max_n));
 
 figure(4);
-hold on;  % 允许在同一张图上绘制多条曲线
+hold on  % 允许在同一张图上绘制多条曲线
 % 绘制攻角
 plot(t_powered, alpha_display, 'r');  % 使用红色
 plot(t_powered(idx_max_alpha), max_alpha, 'o','MarkerFaceColor','k','HandleVisibility','off');
@@ -89,7 +100,7 @@ text(t_powered(idx_max_alpha), max_alpha, sprintf('一级飞行时最大攻角:\
 plot(t_powered, pitch_display, 'g');  % 使用绿色
 % 绘制弹道倾角
 plot(t_powered, theta_v_display, 'b');  % 使用蓝色
-hold off;  % 结束绘制多条曲线
+hold off  % 结束绘制多条曲线
 
 xlabel('时间/s');
 ylabel('角度/°');
@@ -101,74 +112,74 @@ grid on;
 %% 绘制主动段数据
 figure(5);
 subplot(3,2,1);
-hold on;
+hold on
 plot(t_powered, h_display);
 plotBornoutPoint(t_powered, h_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('高度/km');
 title('高度随时间变化');
 grid on;
 
 subplot(3,2,2);
-hold on;
+hold on
 plot(t_powered, v_display);
 plotBornoutPoint(t_powered, v_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('速度/m/s');
 title('速度随时间变化');
 grid on;
 
 subplot(3,2,3);
-hold on;
+hold on
 plot(t_powered, q_display);
 plot(t_powered(idx_max_q), max_q, 'o','MarkerFaceColor','k','HandleVisibility','off');
 text(t_powered(idx_max_q), max_q, sprintf('一级飞行时最大动压:\n%.2fkPa', max_q));
 plotBornoutPoint(t_powered, q_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('动压/Pa');
 title('动压随时间变化');
 grid on;
 
 subplot(3,2,4);
-hold on;
+hold on
 plot(t_powered, m_display);
 plotBornoutPoint(t_powered, m_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('质量/kg');
 title('质量随时间变化');
 grid on;
 
 subplot(3,2,5);
-hold on;
+hold on
 plot(t_powered, theta_L_display);
 plotBornoutPoint(t_powered, theta_L_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('地理经度/°');
 title('地理经度随时间变化');
 grid on;
 
 subplot(3,2,6);
-hold on;
+hold on
 plot(t_powered, Phi_L_display);
 plotBornoutPoint(t_powered, Phi_L_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('地理纬度/°');
 title('地理纬度随时间变化');
 grid on;
 
 figure(6);
-hold on;
+hold on
 plot(t_powered, n_display);
 plot(t_powered(idx_max_n), max_n, 'o','MarkerFaceColor','k','HandleVisibility','off');
 text(t_powered(idx_max_n), max_n, sprintf('一级飞行时最大法向过载:\n%.2fg', max_n));
 plotBornoutPoint(t_powered, n_display, vec_idx);
-hold off;
+hold off
 xlabel('时间/s');
 ylabel('过载/g');
 title('过载随时间变化');
@@ -178,5 +189,11 @@ end
 function plotBornoutPoint(t, data, indexs)
 for i = 1:length(indexs)
     plot(t(indexs(i)), data(indexs(i)), '*');
+end
+end
+
+function plotBornoutPoint3(X, indexs)
+for i = 1:length(indexs)
+    plot3(X(indexs(i),1), X(indexs(i),3), X(indexs(i),2), '*');
 end
 end
